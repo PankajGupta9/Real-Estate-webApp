a0d842c07fa2cee6b2ee56eae603a754e398644c
Pankaj <pg254289@gmail.com>
2024-11-09 18:01:07 +0530

    create user updare api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 dotenv.config();
@@ -22,6 +23,7 @@ mongoose
 
 
 app.use(express.json());
+app.use(cookieParser());
 
 // Server Initialization
 app.listen(3000, () => {
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,24 @@
 import {useSelector} from 'react-redux';
-
+import {useRef} from 'react';
 
 const Profile = () => {
+  const fileRef = useRef(null)
   const {currentUser} = useSelector((state) => state.user)
+  const imageUrl = currentUser?.avatar || 'ghghb' 
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
          <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
 
          <form className='flex flex-col gap-4'>
-          <img src={currentUser.avatar} alt="profile" 
-            className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
+         <input type="file" ref={fileRef} hidden accept='image/*' />
+         <img onClick={()=>fileRef.current.click()}
+          src={imageUrl} 
+          alt="profile" 
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' 
+        />
+
             <input type="text" placeholder='username' id='username' 
             className='border p-3 rounded-lg' />
             <input type="email" placeholder='email' id='email' 
