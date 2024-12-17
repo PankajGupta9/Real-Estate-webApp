5abb7b586821d0d56cfb2a3a35183ffab4122361
Pankaj <pg254289@gmail.com>
2024-12-17 12:52:24 +0530

    create user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,6 +2,7 @@ import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import {errorHandler}  from '../utils/error.js';
 import jwt from 'jsonwebtoken';
+import Listing from '../models/listing.model.js'
 
 export const test = (req, res) => {
     res.json({
@@ -23,14 +24,15 @@ export const updateUser = async (req, res, next) => {
       return res.status(404).json({ error: "User not found" });
     }
 
-    const { username, password, avatar } = req.body;
+    const { username, email, password, avatar } = req.body;
 
     const updates = {
       username: username || existingUser.username,
+      email: email || existingUser.email,
       avatar: avatar || existingUser.avatar,
       password: password ? bcryptjs.hashSync(password, 10) : existingUser.password,
     };
-
+   console.log('email', req.body.email);
     const updatedUser = await User.findByIdAndUpdate(userId, { $set: updates }, { new: true });
 
     if (!updatedUser) {
@@ -72,6 +74,22 @@ export const deleteUser = async(req,res,next) => {
 
 
 
+export const getUserListings = async (req,res,next) => {
+  console.log('req.params.id:', req.params.id);
+  console.log('req.user.id:', req.user.id);
+
+  if(req.user.id === req.params.id) {
+
+  try{
+    const listings = await Listing.find({userRef: req.params.id});
+    res.status(200).json(listings);
+  }catch (error){
+   next(error)
+  }
+  }else{
+    return next(errorHandler(401, 'You can only view your own listing!'))
+  }
+}
 
 
 
